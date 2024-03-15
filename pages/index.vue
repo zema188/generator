ed2f4272b50667f839a78528f7660133fcb96e6b
zemail <artem.zimin02@gmail.com>
2024-03-15 16:35:27 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>
+            Генератор семантического кода
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    font-weight: bold;
+    font-size: 30px;
+}
+</style>
